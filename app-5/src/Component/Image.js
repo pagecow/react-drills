49875fc195111ccb69122491b0768dd33378d96f
pagecow <pagecow@gmail.com>
2019-09-24 16:00:34 -0600

    finished app-5

---
+++ app-5/src/Component/Image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Image = () => {
+    return(
+        <img className='image' src="https://i.pinimg.com/236x/36/a0/b1/36a0b1ed71713bd21dbb0b9cc3ed6464--memes.jpg" />
+    )
+}
+
+export default Image
