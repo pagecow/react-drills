c59a5e572afc1914d15f5ed563d775959883b81f
pagecow <pagecow@gmail.com>
2019-09-24 16:51:37 -0600

    finished app-6

---
+++ app-6/src/Component/ToDoList.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class ToDoList extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            myTaskInputs: '',
+            myCurrentTasks: []
+        }
+    }
+
+    handleChange = (value) => {
+        this.setState({
+            myTaskInputs: value
+        })
+    }
+
+    handleCurrentTasks = (value) => {
+        this.setState({
+            myCurrentTasks: [...this.state.myCurrentTasks, value]
+        })
+    }
+
+    render(){
+        console.log(this.state.myCurrentTasks)
+        return(
+            <div>
+                <h1>My to-do list:</h1>
+                <input onChange={e => this.handleChange(e.target.value)} type='text'/>
+                <button onClick={e => this.handleCurrentTasks(this.state.myTaskInputs)}>Add</button>
+                <div>{this.state.myCurrentTasks.map(e => {
+                    return <h2>{e}</h2>
+                })}</div>
+            </div>
+        )
+    }
+}
+
+export default ToDoList
