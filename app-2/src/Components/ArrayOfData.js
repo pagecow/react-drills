5f7434799f542448ca1830c252d60f5d443d37b3
pagecow <pagecow@gmail.com>
2019-09-24 14:19:44 -0600

    finished app 2

---
+++ app-2/src/Components/ArrayOfData.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ArrayOfData = () => {
+    return (
+        <div>
+            <h2>spaghetti</h2>
+            <h2>ice cream</h2>
+            <h2>spaghetti</h2>
+            <h2>sushi</h2>
+            <h2>cheese</h2>
+        </div>
+    )
+}
+
+export default ArrayOfData
