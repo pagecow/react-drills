63a0b70b50452d3b24d3f94cc1c540940e60e798
pagecow <pagecow@gmail.com>
2019-10-22 15:25:27 -0600

    app-5 finished

--- app-5/src/Component/Image.js
+++ app-5/src/Component/Image.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Image = () => {
+    const [image, setImage] = useState('https://i.pinimg.com/236x/36/a0/b1/36a0b1ed71713bd21dbb0b9cc3ed6464--memes.jpg')
+    
     return(
-        <img className='image' src="https://i.pinimg.com/236x/36/a0/b1/36a0b1ed71713bd21dbb0b9cc3ed6464--memes.jpg" />
+        <img className='image' src={image} />
     )
 }
 
