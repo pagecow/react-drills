0242c2fa97a2f6be30c888bfc1ad434d61aa739e
pagecow <pagecow@gmail.com>
2019-10-22 14:44:13 -0600

    app 3 finished

--- app-3/src/Components/TextAndFilter.js
+++ app-3/src/Components/TextAndFilter.js
@@ -1,37 +1,18 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class TextAndFilter extends Component {
-    constructor(){
-        super();
+const TextAndFilter = () => {
+    const [array, setArray] = useState(['spaghetti', 'pasta', 'the Joker', 'crying horses', 'vestibule maneuvers']);
+    const [filteredStrings, setFilteredStrings] = useState('')
 
-        this.state = {
-            filterString: "",
-            myArray: ['food', 'money', 'cows', 'dentistry', 'stickiness']
-        }
-    }
+    const thingsToDisplay = array.filter(e => {return e.includes(filteredStrings)})
 
-    changeHandler = (filter) => {
-        this.setState({
-            filterString: filter,
-        })
-    }
-
-    render(){
-        let thingsToDisplay = this.state.myArray
-            .filter((e, i) => {
-                return e.includes(this.state.filterString);
-            })
-            .map((e, i) => {
-                return <h2 key={i}>{e}</h2>
-            })
-
-        return(
-            <div>
-                <input onChange={e => this.changeHandler(e.target.value)} type="text"></input>
-                {thingsToDisplay}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <input onChange={(e) => setFilteredStrings(e.target.value)}/>
+            {thingsToDisplay.map(e => {return(<h1>{e}</h1>)})}
+            
+        </div>
+    )
 }
 
 export default TextAndFilter
